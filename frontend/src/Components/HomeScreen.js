abe4151d048e5479b327d494bd9935a1c4c1df4c
AnandKrishnanKS <anandakrishnan1000@gmail.com>
2023-05-24 18:40:18 +0530

    changed corosal style

--- frontend/src/Components/HomeScreen.js
+++ frontend/src/Components/HomeScreen.js
@@ -2,34 +2,50 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import data from '../data';
 import CarouselComponent from './CarouselComponent';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
 function HomeScreen() {
   return (
-    <div>
+    <>
     
-      <div>
-        <CarouselComponent cars={data.cars} />
-      </div>
-      <h1>Hyundai</h1>
-      <div className="cars">
-        {data.cars.map((cars) => (
-          <div className="car" key={cars.slug}>
-            <Link to={`/car/${cars.slug}`}>
-              <img src={cars.image} alt={cars.name} />
-            </Link>
-            <div className="car-info">
-              <Link to={`/car/${cars.slug}`}>
-                <p>{cars.name}</p>
-              </Link>
-              <p>
-                <strong>{cars.price} Lakhs onwards</strong>
-              </p>
-              <button>Book Test Drive</button>
+      <Container>
+        <Row>
+          
+            <Col lg={8}>
+              <CarouselComponent cars={data.cars} />
+            </Col>
+          
+        </Row>
+       <Row> 
+        <Col>
+        <h1>Hyundai</h1>
+        </Col>
+       </Row>
+        <Row>
+          <Col>
+            <div className="cars rounded">
+              {data.cars.map((cars) => (
+                <div className="car" key={cars.slug}>
+                  <Link to={`/car/${cars.slug}`}>
+                    <img src={cars.image} alt={cars.name} />
+                  </Link>
+                  <div className="car-info">
+                    <Link to={`/car/${cars.slug}`}>
+                      <p>{cars.name}</p>
+                    </Link>
+                    <p>
+                      <strong>{cars.price} Lakhs onwards</strong>
+                    </p>
+                    <Button className='btn' >Book Test Drive</Button>
+                  </div>
+                </div>
+              ))}
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
+          </Col>
+        </Row>
+      </Container>
+
+    </>
   );
 }
 
